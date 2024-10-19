3d6323899fd54aeff033103c950aa975adae25f6
gvpmoneesha <moneeshakavindi@gmail.com>
2024-10-19 14:08:17 +0530

    vehicle model was craeted

---
+++ backend/model/vehicle.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const vehicleShema = new mongoose.Schema({
+  no: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  cNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  dob: {
+    type: Date,
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  nic: {
+    type: String,
+    required: true,
+  },
+
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  model: {
+    type: String,
+    required: true,
+  },
+});
+
+const Vehicle = mongoose.model("Vehicle", vehicleShema);
+export default Vehicle;
