0ec6807cf86b59d61c608f9ceb003028079ceea1
gvpmoneesha <moneeshakavindi@gmail.com>
2025-01-31 20:28:06 +0530

    completed driver dashboard

---
+++ frontend/src/components/DashOfficersView.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const DashOfficersView = () => {
+  return (
+    <div>
+      <div>Hi</div>
+    </div>
+  );
+};
--- frontend/src/pages/DriverDashboard.jsx
+++ frontend/src/pages/DriverDashboard.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import Header from "../components/Header";
+import { Sidebar } from "flowbite-react";
+import { HiShoppingBag } from "react-icons/hi";
+import { DashOfficersView } from "../components/DashOfficersView";
+import { DashFineView } from "../components/DashFineView";
 
 export const DriverDashboard = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <div>
-      <div>Hi</div>
+      <div>
+        <div className="bg-teal-400 ">
+          <Header />
+        </div>
+
+        <div className="flex flex-col  sm:flex-row">
+          <div>
+            <Sidebar aria-label="Sidebar with logo branding example">
+              <Sidebar.Items className="sm:min-h-screen">
+                <Sidebar.ItemGroup className="p-0">
+                  <Link to="/driverdashboard?dash=fine-view">
+                    <Sidebar.Item href="#" icon={HiShoppingBag}>
+                      Past Violation Information
+                    </Sidebar.Item>
+                  </Link>
+                  <Link to="/driverdashboard?dash=officer-view">
+                    <Sidebar.Item icon={HiShoppingBag}>
+                      Police officer information
+                    </Sidebar.Item>
+                  </Link>
+                  <Sidebar.Item href="#" icon={HiShoppingBag}>
+                    Pay Payment
+                  </Sidebar.Item>
+                </Sidebar.ItemGroup>
+              </Sidebar.Items>
+            </Sidebar>
+          </div>
+
+          <div className="   w-full sm:max-w-8xl bg-cover  bg-no-repeat bg-center relative overflow-hidden bg-[url('G:\miniproject\frontend\src\assets\signup.jpg')]  ">
+            <div className="bg-slate-200  bg-opacity-80">
+              {(searchParams.get("dash") === "fine-view" && <DashFineView />) ||
+                (searchParams.get("dash") === "officer-view" && (
+                  <DashOfficersView />
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
